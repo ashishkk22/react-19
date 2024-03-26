bca4cf0f60a7bbc630c83aaf61a34cdee7466782
ashishkk22 <ashishkachhadiya22@gmail.com>
2024-03-27 00:13:21 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+import SideMenu from "./components/Menu/Menu";
+
+const App = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+    <SideMenu />
+  </div>
+);
+
+export default App;
